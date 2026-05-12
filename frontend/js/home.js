65936a7d41589fef77d03827621a72eb1e5fb749
Supermyskin <94800933+Supermyskin@users.noreply.github.com>
2026-05-12 16:29:49 +0300

    Update API_URL to use production endpoint

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,6 +1,6 @@
 const userName = localStorage.getItem('userName');
 const userID = localStorage.getItem('userID');
-const API_URL = 'http://127.0.0.1:3000';
+const API_URL = 'https://questure.onrender.com';
 
 const thresholds = [
     { level: 1, xp: 0 },
